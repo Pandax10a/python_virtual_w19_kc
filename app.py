6cd12dd47afd4fc7284d5f662af475ec5e6b864c
panda1 <blueblue0011@gmail.com>
2022-09-24 12:56:41 -0700

    added try and except

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ import wikipedia
 # user_input = input('which page you want to see? ')
 # print(search(wikipedia.page(user_input)))
 
-user_input = wikipedia.page(input('what do you want to see? '))
+try:
+    user_input = wikipedia.page(input('what do you want to see? '))
+    print(user_input.title)
+    print(user_input.content)
+except wikipedia.exceptions.PageError:
+    print('does not exist')
 
-print(user_input.title)
-print(user_input.content)
+
